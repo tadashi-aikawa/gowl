93ea1d2bbcdea2bb8ee28efed72c6665ce564f9c
tadashi-aikawa <syou.maman@gmail.com>
2018-09-18 22:42:46 +0900

    Use go-github

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/github"
+)
 
 func main() {
-	res, err := SearchRepositories("miroir")
+	client := github.NewClient(nil)
+	repos, _, err := client.Search.Repositories(context.Background(), "miroir", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, repo := range res.Items {
-		fmt.Printf("★%-8v%-15v%-15v%-45v%v\n", repo.StargazersCount, repo.ID, repo.Language, repo.FullName, repo.UpdatedAt)
+	for _, repo := range repos.Repositories {
+		fmt.Printf("%v\n", repo.GetFullName())
 	}
+
+	fmt.Printf("â˜…%-8v%-15v%-15v%-45v%v\n", repo.StargazersCount, repo.ID, repo.Language, repo.FullName, repo.UpdatedAt)
 }
