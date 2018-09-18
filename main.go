ff254436993eb9eb569b9e7492496600545e20ee
tadashi-aikawa <syou.maman@gmail.com>
2018-09-19 00:51:46 +0900

    Support for arguments

--- main.go
+++ main.go
@@ -4,15 +4,34 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/BurntSushi/toml"
+	"github.com/docopt/docopt-go"
 	"github.com/google/go-github/github"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 )
 
+const version = "0.1.0"
+const usage = `Gowl.
+
+Usage:
+  gowl <word>
+  gowl -h | --help
+  gowl --version
+
+Options:
+  <word>        Search word for repository.
+  -h --help     Show this screen.
+  --version     Show version.
+  `
+
 const configPath = ".gowlconfig"
 
+type args struct {
+	Word string `docopt:"<word>"`
+}
 type service struct {
 	Token string
 }
@@ -39,22 +58,44 @@ func createConfig() (config, error) {
 	return conf, nil
 }
 
+func createArgs(usage string, argv []string, version string) (args, error) {
+	parser := &docopt.Parser{
+		HelpHandler:  docopt.PrintHelpOnly,
+		OptionsFirst: false,
+	}
+
+	opts, err := parser.ParseArgs(usage, argv, version)
+	if err != nil {
+		return args{}, errors.Wrap(err, "Fail to parse arguments.")
+	}
+
+	var args args
+	opts.Bind(&args)
+
+	return args, nil
+}
+
 func main() {
+	args, err := createArgs(usage, os.Args[1:], version)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "Fail to create arguments."))
+	}
+
 	config, err := createConfig()
 	if err != nil {
-		log.Fatal(errors.Wrap(err, "Failure to load `.config`."))
+		log.Fatal(errors.Wrap(err, "Fail to load `.config`."))
 	}
 
 	client := createGithubClient(config.GitHub.Token)
 
-	repos, _, err := client.Search.Repositories(context.Background(), "miroir", nil)
+	repos, _, err := client.Search.Repositories(context.Background(), args.Word, nil)
 	if err != nil {
-		log.Fatal(errors.Wrap(err, "Failure to search repositories."))
+		log.Fatal(errors.Wrap(err, "Fail to search repositories."))
 	}
 
 	for _, repo := range repos.Repositories {
 		fmt.Printf(
-			"★%-8v%-15v%-15v%-45v%-15v\n",
+			"★%-8v%-15v%-15v%-60v%-15v\n",
 			repo.GetStargazersCount(), repo.GetID(), repo.GetLanguage(), repo.GetFullName(), repo.GetUpdatedAt(),
 		)
 	}
