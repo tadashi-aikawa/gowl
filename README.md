541ccc31161df38b22fc5296b58a70a1beeb33ea
tadashi-aikawa <syou.maman@gmail.com>
2018-09-19 00:10:52 +0900

    Use OAuth2

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# gowl
+gowl
+====
+
 Go tools for GitHub and Bitbucket
+
+
+Install
+-------
+
+TODO...
+
+
+Usage
+-----
+
+TODO...
+
+
+Quick start
+-----------
+
+### Create `.gowlconfig`
+
+`.gowlconfig` is a TOML file.
+
+```toml
+[github]
+token = <your github token>
+```
+
+### Run
+
+TODO...
--- main.go
+++ main.go
@@ -3,20 +3,59 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
+	"github.com/BurntSushi/toml"
 	"github.com/google/go-github/github"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
 )
 
+const configPath = ".gowlconfig"
+
+type service struct {
+	Token string
+}
+type config struct {
+	GitHub service
+}
+
+func createGithubClient(token string) *github.Client {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+
+	tc := oauth2.NewClient(ctx, ts)
+	return github.NewClient(tc)
+}
+
+func createConfig() (config, error) {
+	var conf config
+	if _, err := toml.DecodeFile(configPath, &conf); err != nil {
+		return config{}, err
+	}
+
+	return conf, nil
+}
+
 func main() {
-	client := github.NewClient(nil)
+	config, err := createConfig()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "Failure to load `.config`."))
+	}
+
+	client := createGithubClient(config.GitHub.Token)
+
 	repos, _, err := client.Search.Repositories(context.Background(), "miroir", nil)
 	if err != nil {
-		panic(err)
+		log.Fatal(errors.Wrap(err, "Failure to search repositories."))
 	}
 
 	for _, repo := range repos.Repositories {
-		fmt.Printf("%v\n", repo.GetFullName())
+		fmt.Printf(
+			"★%-8v%-15v%-15v%-45v%-15v\n",
+			repo.GetStargazersCount(), repo.GetID(), repo.GetLanguage(), repo.GetFullName(), repo.GetUpdatedAt(),
+		)
 	}
-
-	fmt.Printf("â˜…%-8v%-15v%-15v%-45v%v\n", repo.StargazersCount, repo.ID, repo.Language, repo.FullName, repo.UpdatedAt)
 }
